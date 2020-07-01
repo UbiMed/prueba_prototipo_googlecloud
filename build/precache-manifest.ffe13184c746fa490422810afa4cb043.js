73f2fe21384fbb471dcc7c6aedbe61332db9009d
edwardsifi <edwardsifi0@gmail.com>
2020-07-01 14:17:08 -0600

    prueba protitipo
    google cloud

---
+++ build/precache-manifest.ffe13184c746fa490422810afa4cb043.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8bf171b8d8862f5bb9482ceb6eb85cd0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0698c74d6c49ff8c3cd8",
+    "url": "/static/css/2.0dc910f2.chunk.css"
+  },
+  {
+    "revision": "a21a1923f723fc0d9cde",
+    "url": "/static/css/main.02fe1fc2.chunk.css"
+  },
+  {
+    "revision": "0698c74d6c49ff8c3cd8",
+    "url": "/static/js/2.c0c25bd1.chunk.js"
+  },
+  {
+    "revision": "a21a1923f723fc0d9cde",
+    "url": "/static/js/main.42e251c7.chunk.js"
+  },
+  {
+    "revision": "46f6293c3ca389710bde",
+    "url": "/static/js/runtime~main.cd912dc6.js"
+  },
+  {
+    "revision": "7672211f03abe474a4ae321dc7022a73",
+    "url": "/static/media/pimp.7672211f.svg"
+  },
+  {
+    "revision": "414701bff487824dd2071ca288efaae8",
+    "url": "/static/media/ubimed-logo.414701bf.png"
+  }
+]);
